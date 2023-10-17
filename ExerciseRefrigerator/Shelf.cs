316c3b043c1c17b84bf1ec436d1f16385c889ed2
ayaladamary <ayalad@darkeychana.com>
2023-10-17 13:49:03 +0300

    Added project

---
+++ ExerciseRefrigerator/Shelf.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ExerciseRefrigerator
+{
+   public class Shelf
+    {
+        public static int UniqIdShelf = 0;
+        public int Id { get; }
+        public int Floor { get; set; }
+        public int FreeSpace { get; set; }
+        public List<Item> Items { get; set; }
+        public Shelf(int floor, int freeSpace)
+        {
+            Id = UniqIdShelf++;
+            Floor = floor;
+            FreeSpace = freeSpace;
+            Items = new List<Item>();
+        }
+  
+        public override string ToString()
+        {
+            return $"Shelf number: {Id}\nFloor: {Floor}\nFree space: {FreeSpace}\n";
+        }
+
+    }
+}
